9c69359e55609bab8239749a343c264ff4d6d887
doan hieu nghia <doanhieunghia@gmail.com>
2025-04-07 16:34:47 +0700

    create homepage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { TrangChuComponent } from './trang-chu/trang-chu.component';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter([{ path: '', component: TrangChuComponent }])]
 };
---
+++ src/app/trang-chu/trang-chu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trang-chu',
+  standalone: true,
+  imports: [],
+  templateUrl: './trang-chu.component.html',
+  styleUrl: './trang-chu.component.css'
+})
+export class TrangChuComponent {
+
+}
